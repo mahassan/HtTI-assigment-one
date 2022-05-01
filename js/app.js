84b3b5ff796b89391121d0f33efaf1f097383cd8
Ammar Hassan <ammar@ammarhassan.co.uk>
2022-05-02 00:51:58 +0100

    reverting to http as free plain doesnt give secure

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ const city = document.querySelector(".city");
 const country = document.querySelector(".country");
 const temp = document.querySelector(".temp");
 
-fetch(`https://api.weatherstack.com/current?access_key=${'41947a4e61efe945bc735849649958b0'}&query=${'Manchester'}`)
+fetch(`http://api.weatherstack.com/current?access_key=${'41947a4e61efe945bc735849649958b0'}&query=${'Manchester'}`)
 .then((res)=>{
     return res.json();
 }).then((data)=>{
